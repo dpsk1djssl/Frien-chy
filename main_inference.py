eeef53cb65de70f6b13592ef0b8d5d12b0ce58c0
SweetFriedPotato <123169135+SweetFriedPotato@users.noreply.github.com>
2025-09-09 13:37:46 +0900

    langgraph memory feature added

--- main_inference.py
+++ main_inference.py
@@ -73,7 +73,7 @@ def build_qdrant_vectorstore(embeddings):
 
 def build_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-2.5-pro",
         google_api_key=os.getenv("GEMINI_API_KEY"),
         temperature=0.1,
     )
