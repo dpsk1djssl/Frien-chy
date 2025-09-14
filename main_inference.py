12b13835df5be4a88e75527ee9d6d00efa1d5411
mathaiaaaa <dpsk1djssl@ewhain.net>
2025-09-14 15:41:17 +0900

    Update main_inference.py

--- main_inference.py
+++ main_inference.py
@@ -68,7 +68,7 @@ def build_qdrant_vectorstore(embeddings):
 
 def build_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-1.5-pro",
+        model="gemini-2.5-pro",
         google_api_key=os.getenv("GEMINI_API_KEY"),
         temperature=0.1,
     )
